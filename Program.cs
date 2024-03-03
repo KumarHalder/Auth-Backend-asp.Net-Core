b3ff0679e1edcc25a4299cc35be80577aa121e06
Kumar Halder <kumar.halder@5x5.ai>
2024-03-03 12:02:15 -0500

    readme added, small refactor.

--- Program.cs
+++ Program.cs
@@ -3,18 +3,15 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
 builder.Services.AddControllers();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.Cookie.Name = "auth";
-        
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30); // access duration for particular cookie
         options.LoginPath = "/api/login"; // Specify your login path
-        options.AccessDeniedPath = "/api/accessdenied"; // Specify your access denied path
+        options.AccessDeniedPath = "/api/access-denied"; // Specify your access denied path
     });
 
 // Configure CORS to allow requests from the React app
@@ -22,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(build =>
     {
-        build.WithOrigins("http://localhost:3000") // Replace with your React app's URL
+        build.WithOrigins("http://localhost:3000") // Replace with your frontend app's URL
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials(); // If you need to include credentials (e.g., cookies)
---
+++ README.md
@@ -0,0 +1,9 @@
+Backend project in asp.net core on authentications. This project demonstrates different authentication and authorizations in asp.net core.
+
+Checkout different branches for specific authentication and authorization techniques. This codebase is for education purpose, and code patterns are not recommended in production use and implementation.
+
+To run the project locally,
+
+```bash
+ $dotnet run
+```
